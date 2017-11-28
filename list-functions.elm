bc9ffc52e8675b7b3bde78f4613904a85672e856
CJ Moore <mooc123@gmail.com>
2017-11-28 15:59:46 -0700

    add list functions

---
+++ list-functions.elm
@@ -0,0 +1,253 @@
+import Html exposing (text)
+
+list1 = [1,2,3,4,5]
+list2 = []
+
+isEmpty : List a -> Bool
+isEmpty list = 
+   List.foldr (\item bool -> False ) True list
+
+
+-- 1) Recursive
+-- 2) Using Foldl
+length : List a -> Int
+length list = 
+  case list of
+    [] ->
+      0
+    x::tail ->
+      1 + (length tail)
+      
+      
+reverse1 : List a -> List a
+reverse1 list = 
+  List.foldl (::) [] list
+      
+reverse2 : List a -> List a
+reverse2 list =
+  case list of 
+    [] -> 
+      []
+    x::tail ->
+      (reverse2 tail) ++ [x]
+      
+member1 : List a -> a -> Bool
+member1 list member = 
+  List.foldr (\item bool -> if item == member then True else bool) False list  
+
+member2 : List a -> a -> Bool
+member2 list isIncluded = 
+  case list of
+    [] -> 
+      False
+    x::tail ->
+      if x == isIncluded then
+        True
+      else
+        member2 tail isIncluded
+        
+        
+head : List a -> Maybe a 
+head list = 
+  case list of
+    [] ->
+      Nothing
+    x::tail ->
+     Just x
+     
+tail : List a -> Maybe (List a)
+tail list = 
+  case list of 
+    [] ->
+      Nothing
+    x::tail ->
+      Just tail
+                
+                
+filter1 : (a -> Bool) -> List a -> List a
+filter1 f list = 
+  List.foldr (\item newList -> if (f item) then newList ++ [item] else newList) [] list
+
+filter2 : (a -> Bool) -> List a -> List a
+filter2 f list = 
+  case list of
+    [] ->
+      []
+    x::tail ->
+      if (f x) then
+        (filter2 f tail) ++ [x]
+      else
+        filter2 f tail 
+        
+
+isEven : Int -> Bool
+isEven num = 
+  (num % 2) == 0
+  
+  
+take1 : Int -> List a -> List a
+take1 num list = 
+  List.foldl (\item newList 
+                -> if ((List.length newList) < num) then
+                       newList ++ [item]
+                    else
+                      newList
+             ) [] list       
+             
+   
+take2 : Int -> List a -> List a
+take2 num list =
+  case list of 
+    [] ->
+      []
+    x::tail ->
+      if num == 0 then
+        []
+      else
+        [x] ++ (take2 (num - 1) tail)
+        
+        
+take3 : Int -> List a -> List a
+take3 num list = 
+  List.foldl (\item listTuple
+                -> if (Tuple.second listTuple) == 0 then
+                     (Tuple.first listTuple, 0)
+                   else
+                     (((Tuple.first listTuple) ++ [item]), ((Tuple.second listTuple) - 1))
+             ) ([], num) list
+  |> Tuple.first            
+          
+drop1 :  Int -> List a -> List a       
+drop1 num list = 
+  List.foldl (\item listTuple
+                -> if (Tuple.second listTuple) < num then
+                     (Tuple.first listTuple, (1+ Tuple.second listTuple))
+                   else
+                     (((Tuple.first listTuple) ++ [item]), num)
+             ) ([], 0) list
+  |> Tuple.first   
+  
+  
+drop2 : Int -> List a -> List a
+drop2 num list =
+  case list of 
+    [] ->
+      []
+    x::tail ->
+      if num == 0 then
+         list
+      else
+        drop2 (num - 1) tail  
+
+
+concat : List (List a) -> List a
+concat listOfLists = 
+  List.foldl (\item list -> list ++ item) [] listOfLists
+  
+  
+concat2 : List (List a) -> List a
+concat2 listOfLists = 
+  case listOfLists of 
+    [] ->
+      []
+    x::tail ->
+      x ++ (concat2 tail)
+  
+(+++) : List a -> List a -> List a
+(+++) listOne listTwo = 
+  case listOne of 
+    [] ->
+      listTwo
+    x::tail ->
+      (::) x ((+++) tail listTwo)
+      
+
+intersperse1 : a -> List a -> List a
+intersperse1 newThing list = 
+  List.foldl (\item newList -> newList ++ [item] ++ [newThing]) [] list 
+  
+  
+intersperse2 : a -> List a -> List a
+intersperse2 addThis list =
+  case list of 
+    [] ->
+      []
+    x::tail ->
+      [x] ++ [addThis] ++ (intersperse2 addThis tail)
+
+--length [1, 2]
+--1 + length [1]
+--1 + 1 + length []
+--1 + 1 + 0
+
+partition1 : (a -> Bool) -> List a -> (List a, List a)
+partition1 f list = 
+ List.foldl (\item newTuple -> if (f item) then 
+                                 (((Tuple.first newTuple) ++ [item]), (Tuple.second newTuple))
+                               else
+                                 ((Tuple.first newTuple), ((Tuple.second newTuple) ++ [item]))
+            ) ([],[]) list
+            
+            
+partition2 : (a -> Bool) -> List a -> (List a, List a)
+partition2 f list = 
+  case list of
+    [] ->
+      ([], [])
+    x::tail ->
+      let
+        (trueInTail, falseInTail) = partition2 f tail
+      in
+        if (f x) then
+          (([x] ++ trueInTail), falseInTail)
+        else
+          (trueInTail, ([x] ++ falseInTail))
+          
+          
+unzip1 : List (a, b) -> (List a, List b)
+unzip1 list = 
+  List.foldl (\item newTuple -> ( (Tuple.first newTuple ++ [Tuple.first item])
+                                , (Tuple.second newTuple ++ [Tuple.second item])
+                                ) ) ([], []) list
+                                
+                                
+unzip2 : List (a, b) -> (List a, List b)
+unzip2 list = 
+  case list of
+    [] ->
+      ([], [])
+    x::tail ->
+      let
+        (firstPart, secondPart) = unzip2 tail
+      in
+        (([(Tuple.first x)] ++ firstPart ), ([Tuple.second x] ++ secondPart))
+  
+map1 : (a -> b) -> List a -> List b
+map1 f list = 
+  List.foldr (\item newList -> [(f item)] ++ newList) [] list
+
+mapFirst2 : (a -> b) -> List a -> List b
+mapFirst2 f list = 
+  case list of
+    [] ->
+      []
+    x::tail ->
+      [(f x)] ++ mapFirst2 f tail
+      
+ 
+output = 
+  mapFirst2 timesTwo list1
+  
+  
+  
+timesTwo : Int -> Int
+timesTwo num = 
+  num * 2
+  
+-- partition, unzip, map, map2, filterMap, concatMap, indexedMap, sum, product, maximum, minimum, all, any  
+  
+
+  
+  
+main =
+  text (toString output)
